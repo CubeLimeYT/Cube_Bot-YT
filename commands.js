4d94bd12019cc5431877db24451c06ded2a42117
StevenAprentisCodeur <41762297+StevenAprentisCodeur@users.noreply.github.com>
2018-08-31 21:02:38 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -9,23 +9,23 @@ module.exports = {
 	'ban': ban
 }
 
-function ban(msg){
-	if ( !msg.member.hasPermission('BAN_MEMBERS')){
-		return msg.channel.send(`Vous n'avez pas la permission de ban`);
+function ban(message){
+	if ( !message.member.hasPermission('BAN_MEMBERS')){
+		return message.channel.send(`Vous n'avez pas la permission de ban`);
 	}
-	let memberToBan = msg.mentions.members.first();
-	if(memberToBan && memberToBan.bannable && (msg.member.highestRole.calculatedPosition >
-            memberToBan.highestRole.calculatedPosition || msg.guild.ownerID == msg.author.id)){
-		let reason = tool.parseOptionArg('raison', msg.content);
-	    let days = parseInt(tool.parseOptionArg('days', msg.content))
+	let memberToBan = message.mentions.members.first();
+	if(memberToBan && memberToBan.bannable && (message.member.highestRole.calculatedPosition >
+            memberToBan.highestRole.calculatedPosition || message.guild.ownerID == message.author.id)){
+		let reason = tool.parseOptionArg('raison', message.content);
+	    let days = parseInt(tool.parseOptionArg('days', message.content))
 
 	    let banOptions = {
 	    	days : days ? days : 0,
 	    	reason: reason ? reason: 'none'
 	    };
 	    memberToBan.ban(banOptions);
-	    msg.channel.send(` L\'utilisateur ${memberToBan} à bien été bani`)
+	    message.channel.send(` L\'utilisateur ${memberToBan} à bien été bani`)
 	}else{
-        msg.channel.send(`L\'utilisateur ${memberToBan} ne peut être ban`)
+        message.channel.send(`L\'utilisateur ${memberToBan} ne peut être ban`)
     }
 }
