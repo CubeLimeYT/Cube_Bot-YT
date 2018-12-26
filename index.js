b0a7b445d085dc3bd0710f1d3a745861f25f4830
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-12-26 14:06:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('ready', () => {
 
 bot.on('message',async message => {
     if(message.content.startsWith(config.prefix + "restart")) {
-	if(message.author.id !== "307231625459007488") return message.reply('Vous n\'êtes pas le propriétaire du bot');
+	if(message.author.id !== "307231625459007488" || message.author.id !== "372099632173416449") return message.reply('Vous n\'êtes pas le propriétaire du bot');
         message.channel.send('**Redémarrage**').then(msg => {
             setTimeout(() => {
                msg.edit('**Redémarrage..**');
