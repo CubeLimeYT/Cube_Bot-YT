56babd2e209362e7047103cd1e564d626788646a
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-12-30 15:52:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ bot.on('message', function (message) {
 	
 	if(message.content === config.prefix + 'sendDM'){
 		 message.author.createDM().then(channel => {
-		 	message.send("TEST");
+		 	channel.send("TEST");
 		 })
 		message.reply("check dm")
 		.catch(console.error, channel => message.reply("impossible de vous envoyez le dm"));
