71ff813b3cd3bd1da2bbada0cc381576d2e6280f
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-12-30 15:49:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ bot.on('message', function (message) {
 	if(message.content === config.prefix + 'sendDM'){
 		 message.author.createDM().then(channel => {
 		 	message.channel.send("TEST");
-		 })message.reply("check dm").catch(console.error, channel => message.reply("impossible de vous envoyez le dm"));
+		 })
+		message.reply("check dm")
+		.catch(console.error, channel => message.reply("impossible de vous envoyez le dm"));
 	}
 	
 	if (message.content === config.prefix + 'Administration') {
