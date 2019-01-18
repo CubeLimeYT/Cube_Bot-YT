f16b903c6e17b052f6d2a08c8af6c02826956dd4
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-18 23:36:39 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,11 +4,16 @@ module.exports.run = async (bot, message, args) => {
     if(!args[1]) retrun message.reply("posez une vrai question");
     
     let answer = ["Oui", "Non", "Essaye plus tard", "Essaye encore", "C'est ton destin", "Impossinle", "Sans aucun doute", "Pas d'avis", "Repose ta question"]
-    let result = Math.floor((Math.random() * answer.lenght));
+    let result = Math.floor((Math.random() * answer.lenght) + 0);
     
     let question = args.slice(1).join(" ");
     
-    message.channel.send(answer[result]);
+    const ballembed = new Discord.RichEmbed()  
+    .setColor("#FF9900") 
+    .addField(args, answer[result]) 
+    .addField("Réponse:", )
+    
+    message.channel.send({embed: ballembed});
 }
 module.exports.help = {
   name: "8ball"
