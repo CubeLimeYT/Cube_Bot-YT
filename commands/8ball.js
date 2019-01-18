3b3adeb3807a91df8afca0ffea95c5e2565e8610
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-19 00:00:10 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,20 +2,26 @@ const Discord = require("discord.js")
 const config = require("././config.json");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[1]) retrun message.reply("posez une vrai question");
-
+    let args = message.content.split(" ").slice(1);
+    let tte = args.join(" ")
+    if(!tte){
+        return message.reply(" posez une vrai question")
+    };
     
-    let answer = ["Oui", "Non", "Essaye plus tard", "Essaye encore", "C'est ton destin", "Impossible", "Sans aucun doute", "Pas d'avis", "Repose ta question"]
-    let result = Math.floor((Math.random() * answer.length));
+    var replys = [
+        "Oui",
+        "Non",
+        "Je ne sais pas",
+        "Peut être"
+        ];
     
-    let question = args.slice(1).join(" ");
+    let reponse = (replys[Math.floor(Math.random() * replys.length)])
     
-    const ballembed = new Discord.RichEmbed()  
-    .setColor("#FF9900") 
-    .addField("Question:", question)
-    .addField("Réponse", answer[result]) 
-    
-    message.channel.send({embed: ballembed});
+    var bembed = new Discord.RichEmbed()
+    .setDescription(":8ball: 8ball")
+    .addField("Question", tte)
+    .addField("Réponse", reponse)
+    message.channel.sendEmbed(bembed);
 }
 module.exports.help = {
   name: "8ball"
