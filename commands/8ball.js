3fff9913caa2e359ba2b10d37825a511884b6173
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-18 23:20:15 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-    if(args[2] === "help") return message.channel.send()
     if(!args[1]) retrun message.reply( posez une vrai question)
     
     let answer = ["Oui", "Non", "Essaye plus tard", "Essaye encore", "C'est ton destin", "Impossinle", "Sans aucun doute", "Pas d'avis", "Repose ta question"]
