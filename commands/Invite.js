97e19fbf51efd739b485d509f77a9c39c982d3b1
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-19 08:39:40 +0100

    Create Invite.js

---
+++ commands/Invite.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) {
+        let InviteEmbed = new Discord.RichEmbed()
+        .setDescription("Voici le lien pour m'inviter dans ton serveur :wink: ")
+        .setColor('#5EGT5B')
+        .addField('https://discordapp.com/oauth2/authorize?client_id=479022698303848459&scope=bot&permissions=2146958847',"Voilà c'est cadeau :wink: ")
+        .setFooter('Voila si tu ne comprend vraiment rien contacte moi ==> Cube_Lime YT#3607')
+        message.channel.send(InviteEmbed);
+
+}
+module.exports.help = {
+    name: "Invite",
+    aliases: ["invite", "inv"]
+}
