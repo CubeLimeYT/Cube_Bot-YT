8e806fab5b1b0216a450810b0cea9057f0cf6500
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-19 08:40:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -123,27 +123,6 @@ if(message.content.startsWith(config.prefix + "restart")) {
         message.channel.send(youtubeEmbed);
     	}
     }
-
-    if (message.content === config.prefix + 'Invite') {
-            
-    if(message.channel.type === 'dm'){
-        message.author.createDM().then(channel => {
-	let InviteEmbed = new Discord.RichEmbed()
-        .setDescription("Voici le lien pour m'inviter dans ton serveur :wink: ")
-        .setColor('#5EGT5B')
-        .addField('https://discordapp.com/oauth2/authorize?client_id=479022698303848459&scope=bot&permissions=2146958847',"Voilà c'est cadeau :wink: ")
-        .setFooter('Voila si tu ne comprend vraiment rien contacte moi ==> Cube_Lime YT#3607')     
-        	channel.send(InviteEmbed)
-	});
-    }else{
-	let InviteEmbed = new Discord.RichEmbed()
-        .setDescription("Voici le lien pour m'inviter dans ton serveur :wink: ")
-        .setColor('#5EGT5B')
-        .addField('https://discordapp.com/oauth2/authorize?client_id=479022698303848459&scope=bot&permissions=2146958847',"Voilà c'est cadeau :wink: ")
-        .setFooter('Voila si tu ne comprend vraiment rien contacte moi ==> Cube_Lime YT#3607') 
-    	message.channel.send(InviteEmbed);
-    }
-}
 	
 	if(message.content === config.prefix + "help"){
 	
