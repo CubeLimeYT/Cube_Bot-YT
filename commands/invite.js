5646f08180d7231a0aa7a15be328a63d8a8b0307
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-19 08:52:31 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+        if(message.channel.type === 'dm'){
+            message.author.createDM().then(channel => {
+        let InviteEmbed = new Discord.RichEmbed()
+        .setDescription("Voici le lien pour m'inviter dans ton serveur :wink: ")
+        .setColor('#5EGT5B')
+        .addField('https://discordapp.com/oauth2/authorize?client_id=479022698303848459&scope=bot&permissions=2146958847',"Voilà c'est cadeau :wink: ")
+        .setFooter('Voila si tu ne comprend vraiment rien contacte moi ==> Cube_Lime YT#3607')
+        channel.send(InviteEmbed);
+            })
+        }
         let InviteEmbed = new Discord.RichEmbed()
         .setDescription("Voici le lien pour m'inviter dans ton serveur :wink: ")
         .setColor('#5EGT5B')
