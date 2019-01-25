270de025bf8a160794b569db55dba9ad97de9cbe
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-25 21:17:34 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!args[0]) return message.channel.send("Non");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
-
+  });
 }
 
 module.exports.help = {
