235e9ce67b414474fc4f011552dff364105c219a
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-01-25 21:16:08 +0100

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu ne peux point utiliser cette commande");
+  if(!args[0]) return message.channel.send("Non");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+
+}
+
+module.exports.help = {
+  name: "clear"
+}
