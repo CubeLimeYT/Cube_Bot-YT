7a53179e8c9832fbdc2786b40e3535b6229c5b24
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-02-21 20:56:30 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async (message, bot) => {
+  let args = message.content.split(" ").slice(1);
+
+		if(!args){
+			let HelpEmbed = new Discord.RichEmbed()
+			.setDescription("Voici la description")
+			.setColor('#1CFF1C')
+			.addField('C*Administration', "Vous donne la page d'Administration")
+			.addField('C*Youtube', 'Vous donneras tout concernant le Youtube Game :) ')
+			.addField('C*Game', 'Vous donneras tout les jeux présent sur le bot')
+			.addField('C*Image', 'Vous donneras les commandes pour avoir des images')
+			.addField('C*Invite', "Vous donneras l'invitation du bot")
+			.setFooter('Support Server : https://discord.gg/pcfYDMK ')
+        		message.channel.send(HelpEmbed);
+		}
+		if(args === "music"){
+      			message.channel.send("HElp music");
+		}
+
+}
+
+module.exports.help = {
+  name: "help"
+}
