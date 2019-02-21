24ee83e7072578f6be024041ccaabc7e3f65cda3
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-02-21 20:24:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,17 +53,80 @@ bot.on('message', function (message) {
 	if(message.content === config.prefix + "help"){
 		if(!args){
 			let HelpEmbed = new Discord.RichEmbed()
-        .setDescription("Voici la description")
-        .setColor('#1CFF1C')
-       	.addField('C*Administration', "Vous donne la page d'Administration")
-        .addField('C*Youtube', 'Vous donneras tout concernant le Youtube Game :) ')
-        .addField('C*Game', 'Vous donneras tout les jeux présent sur le bot')
-        .addField('C*Image', 'Vous donneras les commandes pour avoir des images')
-        .addField('C*Invite', "Vous donneras l'invitation du bot")
-        .setFooter('Support Server : https://discord.gg/pcfYDMK ')
+			.setDescription("Voici la description")
+			.setColor('#1CFF1C')
+			.addField('C*Administration', "Vous donne la page d'Administration")
+			.addField('C*Youtube', 'Vous donneras tout concernant le Youtube Game :) ')
+			.addField('C*Game', 'Vous donneras tout les jeux présent sur le bot')
+			.addField('C*Image', 'Vous donneras les commandes pour avoir des images')
+			.addField('C*Invite', "Vous donneras l'invitation du bot")
+			.setFooter('Support Server : https://discord.gg/pcfYDMK ')
         		message.channel.send(HelpEmbed);
-		}else{
-		console.log(args);
+		}
+		if(args === "music")
+			message.channel.send({embed: {
+      color: 0x00FF00,
+      title: `Music Help
+              ${config.prefix}music | <function>
+              Toute les function:`,
+      fields: [{
+            name: "Play",
+            value: `play <url> ou <recherche> : 
+            Ajouter le(s) song ou la playlist à la file d'attente.`,
+            inline: true
+          },
+          {
+            name: "Skip",
+            value: `skip : Passer le song actuel.`,
+            inline: true
+          },
+          {
+            name: "Pause",
+            value: `pause : Pause le song.`,
+            inline: true
+          },
+          {
+            name: "Resume",
+            value: `resume : Reprendre le song.`,
+            inline: true
+          },
+          {
+            name: "Queue",
+            value: `queue : Afficher le(s) song(s) de la file d'attente.`,
+            inline: true
+          },
+          {
+            name: "Purge",
+            value: `purge : Éffacer le(s) song(s) de la file d'attente.`,
+            inline: true
+          },
+          {
+            name: "np",
+            value: `np : Afficher le titre du song actuel.`,
+            inline: true
+          },
+          {
+            name: "Volume",
+            value: `vol <0-100> : Régler le volume.`,
+            inline: true
+          },
+          {
+            name: "Join",
+            value: `join : Rejoins votre voice channel.`,
+            inline: true
+          },
+          {
+            name: "Leave",
+            value: `leave : Quitte voice channel.`,
+            inline: true
+          }
+       ],
+        timestamp: new Date(),
+      footer: {
+        text: "Fatality Team"
+      }
+      }
+  })
 		}
 	}
 	
