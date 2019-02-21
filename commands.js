38cb4058fcd9950a2afef3da49216dd0da4c72b6
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-02-22 00:34:21 +0100

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,23 @@
+const commandHelp = require("./help.js");
+
+module.exports = {
+   'help': help
+}
+
+function help(msg){
+	let args = msg.content.split(/\s+/).slice(1);
+
+	let helpStr;
+	if(args.length == 1){
+		if(args[0].charAt(0) == config.prefix) //['L', 'efjofefe']
+			args[0] = args[0].slice(1);
+		helpStr = commandHelp[args[0]];
+	    
+	}
+	if(helpStr){
+		helpStr(msg);
+  }
+	else {
+		msg.channel.send();
+   }
+}
