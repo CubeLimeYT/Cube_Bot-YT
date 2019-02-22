32791b6c8246c690c0c06e7311bcbe79919c6f5d
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-02-22 15:30:08 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -8,7 +8,43 @@ module.exports = {
 function help(msg){
 	let args = msg.content.split(/\s+/).slice(1);
 	
-	console.log(args);
+	if(msg.content != args){
+		msg.channel.send(
+				{embed: {
+				    color: 0x00FF00,
+				    title: `Help`,
+				    fields: [{
+					  name: "C*Administration",
+					  value: `Vous donne la page d'Administration`,
+					  inline: true
+					},
+					{
+					  name: "C*Youtube",
+					  value: `Vous donneras tout concernant le Youtube Game :)`,
+					  inline: true
+					},
+					{
+					  name: "C*Game",
+					  value: `Vous donneras tout les jeux présent sur le bot`,
+					  inline: true
+					},
+					{
+					  name: "C*Image",
+					  value: `Vous donneras les commandes pour avoir des images`,
+					  inline: true
+					},
+					{
+					  name: "C*Invite",
+					  value: `Vous donneras l'invitation du bot`,
+					  inline: true
+					}],
+				      timestamp: new Date(),
+				    footer: {
+				      text: "Fatality support https://discord.gg/W2uMAsZ"
+				    }
+			      }
+			  });	
+	}	
 
 	let helpStr;
 	if(args.length == 1){
