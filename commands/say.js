588332f31896dea115a027198d76b8b6a4bff574
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-03-07 23:08:05 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-      message.channel.send("La commande est en cours de développement désoler 😪");
       if(message.guild.roles.find(r => r.name === "Say")){
             if (message.member.roles.some(role => role.name === 'Say')){
                   const sayMessage = args.join(" ");
@@ -11,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
             }else{
                         message.reply("vous n'avez pas le role say pour utiliser cette commande");
 
-            
+            }
       }else{
                   let owner = message.guild.owner;
                   message.channel.send(`${owner} il n'y a pas de role comportant le nom: Say pour autoriser certaines personnes à utiliser cette commande`);
