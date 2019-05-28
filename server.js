15da990e17ed5e55ecd7d6337a6546610450f8bf
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-28 16:59:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -228,13 +228,7 @@ bot.on("message", async message => {
 		.addField('Viens aussi sur son serveur discord', 'https://discord.me/fatalityteam')
 		.addField("Tu peut également l'ajouter en ami sur youtube", "https://youtu.be/join/ct-Py9DY3VEGjY")
 		.setFooter("Allez c'est gratuit pour le moment")
-	    if(message.channel.type === 'dm'){
-		message.author.createDM().then(channel => {
-			channel.send(youtubeEmbed)
-		});
-	    }else{
-		message.channel.send(youtubeEmbed);
-		}
+
     }
     
     //Commande pour les jeux du bot
