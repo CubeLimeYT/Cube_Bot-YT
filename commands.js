c1141929de796f40869cb819026f5017ded23a8f
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-30 21:45:03 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -13,20 +13,17 @@ module.exports = {
 
 function help(msg){
     let args = msg.content.split(/\s+/).slice(1);
-	
-	let user;
-	user = message.author;
-
-let helpStr;
-if(args.length == 1){
-    if(args[0].charAt(0) == config.prefix) //['L', 'efjofefe']
-        args[0] = args[0].slice(1);
-    helpStr = commandHelp[args[0]];
-    
-}
-if(helpStr){
-    helpStr(msg);
-}
+	let user = msg.author;
+	let helpStr;
+	if(args.length == 1){
+		if(args[0].charAt(0) == config.prefix) //['L', 'efjofefe']
+			args[0] = args[0].slice(1);
+		helpStr = commandHelp[args[0]];
+	    
+	}
+	if(helpStr){
+		helpStr(msg);
+  }
 else {
 
 	 	
