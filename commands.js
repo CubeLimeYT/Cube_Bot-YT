46370cb068681dc9d795ecafb7c6a36d0660e703
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-30 14:32:17 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -8,7 +8,9 @@ module.exports = {
 
 function help(msg){
 	let args = msg.content.split(/\s+/).slice(1);
-
+	
+	user = message.author;
+	
 	let helpStr;
 	if(args.length == 1){
 		if(args[0].charAt(0) == config.prefix) //['L', 'efjofefe']
@@ -21,27 +23,30 @@ function help(msg){
   }
 	else {
 		let helpembed = new Discord.RichEmbed()
+	.setThumbnail(user.avatarURL)
         .setDescription('**Mis à jour le 30/05/2019 à 10h18**')
         .setColor('#e11010')
         .addField(`**Voici les commandes disponible pour le moment**`, `**========================================**`)
-        .addField(`<:interdit:583396310745481226> ${config.prefix}**Administration** <:interdit:583396310745481226>`, `Vous donne la page d'Administration`)
+        .addField(`<:interdit:583396310745481226> ${config.prefix}**administration** <:interdit:583396310745481226>`, `Vous donne la page d'Administration`)
         .addBlankField(Boolean)
-        .addField(`**Commandes disponible a tous**`, `**==========================================**`)
-        .addField(`<:ampoule:583583402914086913> ${config.prefix}**Utility** <:ampoule:583583402914086913>`, `Vous donnes les commandes utiles`)
-        .addField(`<:youtube:583396381880877160> ${config.prefix}**Youtube** <:youtube:583396381880877160>`, `Vous donneras tout concernant le Youtube Game`)
-        .addField(`<:manette:583396342022275082> ${config.prefix}**Game** <:manette:583396342022275082>`, `Vous donneras tout les jeux présent sur le bot`)
-        .addField(`<:photo:583587918900363264> ${config.prefix}**Image** <:photo:583587918900363264>`, `Vous donneras les commandes pour avoir des images`)
+        .addField(`**Commandes disponible a tous**`, `**========================================**`)
+        .addField(`<:ampoule:583583402914086913> **${config.prefix}utility** <:ampoule:583583402914086913>`, `Vous donnes les commandes utiles`)
+        .addField(`<:youtube:583396381880877160> **${config.prefix}youtube** <:youtube:583396381880877160>`, `Vous donneras tout concernant le Youtube Game`)
+        .addField(`<:manette:583396342022275082> **${config.prefix}game** <:manette:583396342022275082>`, `Vous donneras tout les jeux présent sur le bot`)
+        .addField(`<:photo:583587918900363264> **${config.prefix}imge** <:photo:583587918900363264>`, `Vous donneras les commandes pour avoir des images`)
         .addBlankField(Boolean)
-        .addField(`**Pour m'ajouter à ton serveur**`, `**================================**`)
-        .addField(`<:lien:583587396386422784> ${config.prefix}**Invite** <:lien:583587396386422784>`, `Vous donneras l'invitation du bot en Mp`)
+        .addField(`**Pour m'ajouter à ton serveur**`, `**========================================**`)
+        .addField(`<:lien:583587396386422784> **${config.prefix}invite** <:lien:583587396386422784>`, `Vous donneras l'invitation du bot en Mp`)
         .addBlankField(Boolean)
-        .addField(`**Commandes Music/Radio**`, "**==================================**")
-        .addField(`<:music:583396423676985515> ${config.prefix}**help music** <:music:583396423676985515> `, `Vous donne toute les informations sur la commande music`)
-        .addField(`<:poste:583396951266033699> ${config.prefix}**help radio** <:poste:583396951266033699> `, `Vous donne toute les informations sur la commande radio`)
+        .addField(`**Commandes Music/Radio**`, "**========================================**")
+        .addField(`<:music:583396423676985515> **${config.prefix}help music** <:music:583396423676985515> `, `Vous donne toute les informations sur la commande music`)
+        .addField(`<:poste:583396951266033699> **${config.prefix}help radio** <:poste:583396951266033699> `, `Vous donne toute les informations sur la commande radio`)
         .addBlankField(Boolean)
-        .addField(`**Pour rejoindre la team, c'est très simple`, "**==============================**")
-        .addField(`Rend toi sur le serveur 💀FataLity Team💀 (utilise la commande **${config.prefix}Invite**)`, `Essaie de parler avec le Fondateur ou Co-Fondateur pour pouvoir rejoindre`)
-        .setFooter("Si tu as des problèmes fait moi en part => Fatality_Cucube#3607")
+        .addField(`**Pour rejoindre la team, c'est très simple**`, "**========================================**")
+        .addField(`Rend toi sur le serveur **💀FataLity Team💀** (utilise la commande **${config.prefix}Invite**)`, `Essaie de parler avec le Fondateur ou Co-Fondateur pour pouvoir rejoindre`)
+        .addField(`:warning: **${config.prefix}help+** :warning:`, `Utilisez cette commmande si vous trouvez des bugs ou si vous avez besoin d'aide`)
+	.setFooter(`En réponse à ${message.author.username}#${message.author.discriminator}`)
+	.setTimestamp()
 
 		msg.channel.send(helpembed);
    }
