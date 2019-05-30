cb8857684d75926ceca27d6fa7469fa7e9f6efd5
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2019-05-30 14:50:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -300,8 +300,8 @@ bot.on("message", async message => {
 	//help+
 	
 	if (message.content === config.prefix + `help+`) {
-		message.channel.send(`Tu peut rejoindre le serveur support du bot avec la commande **${config.prefix}invite**`
-		message.reply(`Sinon tu peut aussi ajouter **Fatality_Cucube#3607** en ami, si tu as besoin de plus d'aide :blush:`
+		message.channel.send(`Tu peut rejoindre le serveur support du bot avec la commande **${config.prefix}invite**`)
+		message.reply(`Sinon tu peut aussi ajouter **Fatality_Cucube#3607** en ami, si tu as besoin de plus d'aide :blush:`)
 		
 	
 	
